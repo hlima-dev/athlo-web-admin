41d1076c160fc773d20d60c40cdee65dbda67db7
Lucas Lima Santos <limas.lucs@gmail.com>
2026-06-09 18:11:09 -0300

    fix: dashboard service extraindo data corretamente

--- src/services/dashboard.ts
+++ src/services/dashboard.ts
@@ -1,6 +1,6 @@
-import { api } from "./api";
+﻿import { api } from "./api";
 
 export async function getDashboard() {
   const response = await api.get("/dashboard");
-  return response.data;
+  return response.data.data;
 }
